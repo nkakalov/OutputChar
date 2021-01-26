072d5da0efd6b9a50b27d3a461e0ac0073cb7e5d
nkakalov <nkakalov>
2021-01-26 16:47:43 +0300

    First save

---
+++ char.js
@@ -0,0 +1,7 @@
+const start = 'Я изучаю JavaScript'
+alert( `${start} 
+В данном предложении ${start.length} символов`)
+console.log(start.toLowerCase()) //Привел всё к нижнему регистру 
+console.log("Вывод 3-го символа:",start.toLowerCase() [2])
+console.log("Вывод 1-го символа:",start.toLowerCase() [0])
+console.log("Вывод последнего символа:",start.toLowerCase() [18])
