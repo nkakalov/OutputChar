4663808748dd759c065108bd776b0c8a903db192
nkakalov <nkakalov>
2021-01-26 17:08:38 +0300

    Second save

--- char.js
+++ char.js
@@ -1,7 +1,6 @@
 const start = 'Я изучаю JavaScript'
 alert( `${start} 
 В данном предложении ${start.length} символов`)
-console.log(start.toLowerCase()) //Привел всё к нижнему регистру 
-console.log("Вывод 3-го символа:",start.toLowerCase() [2])
-console.log("Вывод 1-го символа:",start.toLowerCase() [0])
-console.log("Вывод последнего символа:",start.toLowerCase() [18])
+console.log("Вывод 3-го символа:",start [2])
+console.log("Вывод 1-го символа:",start[0])
+console.log("Вывод последнего символа:",start.slice(-1))
